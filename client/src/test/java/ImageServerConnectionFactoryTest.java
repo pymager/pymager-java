418d770574833b791cbf8c5bdb5e41ebed299500
Sami Dalouche <sami.dalouche@gmail.com>
2009-02-19 03:57:42 +0000

    Merge branch 'master' into trunksami

---
+++ client/src/test/java/ImageServerConnectionFactoryTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ImageServerConnectionFactoryTest {
+
+    @Test
+    public void shouldCreateImageServerConnectionUsingDefaultParameters() {
+	assertTrue(true);
+    }
+}
