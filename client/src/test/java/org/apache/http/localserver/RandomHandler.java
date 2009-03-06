f9ec8cd5c2520e1a86f8152cf9db7ad67f69612a
Sami Dalouche <sami.dalouche@gmail.com>
2009-03-06 01:30:36 +0000

    added license headers

--- client/src/test/java/org/apache/http/localserver/RandomHandler.java
+++ client/src/test/java/org/apache/http/localserver/RandomHandler.java
@@ -1,34 +1,22 @@
-/*
- * $HeadURL: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk/module-client/src/test/java/org/apache/http/localserver/RandomHandler.java $
- * $Revision: 723981 $
- * $Date: 2008-12-06 14:37:59 +0100 (Sat, 06 Dec 2008) $
+/**
+ * ImgServer Java REST Client
+ * Copyright (C) 2008 Sami Dalouche
  *
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * This file is part of ImgServer Java REST Client.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * ImgServer Java REST Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
+ * ImgServer Java REST Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ImgServer.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.apache.http.localserver;
 
 import java.io.IOException;
